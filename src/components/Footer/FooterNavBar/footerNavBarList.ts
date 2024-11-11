c8cf038c573b41ea636770ee9fb251bba2616ab0
staff <masterb21052017@gmail.com>
2024-11-11 12:32:03 +0200

    setting netilfy

--- src/components/Footer/FooterNavBar/footerNavBarList.ts
+++ src/components/Footer/FooterNavBar/footerNavBarList.ts
@@ -1,7 +1,7 @@
 import { NavItemType } from "@/types/fotterNavItems";
 
 export const navItems: NavItemType[] = [
-  { id: 1, label: 'Home', link: '/home' },
+  { id: 1, label: 'Home', link: '/' },
   { id: 2, label: 'Find Books', link: '/find-books' },
   { id: 3, label: 'Trades', link: '/trades' },
   { id: 4, label: 'Messages', link: '/messages' },
