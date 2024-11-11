b925309c7acee2ba4240e6831d692aacdca2406e
staff <masterb21052017@gmail.com>
2024-11-11 18:26:26 +0200

    fix error in linl

--- src/components/Footer/FooterNavBar/footerNavBarList.ts
+++ src/components/Footer/FooterNavBar/footerNavBarList.ts
@@ -2,9 +2,9 @@ import { NavItemType } from "@/types/fotterNavItems";
 
 export const navItems: NavItemType[] = [
   { id: 1, label: 'Home', link: '/' },
-  { id: 2, label: 'Find Books', link: '/find-books' },
-  { id: 3, label: 'Trades', link: '/trades' },
-  { id: 4, label: 'Messages', link: '/messages' },
-  { id: 5, label: 'Features', link: '/features' },
-  { id: 6, label: 'Notification', link: '/notification' },
+  { id: 2, label: 'Find Books', link: '#' },
+  { id: 3, label: 'Trades', link: '#' },
+  { id: 4, label: 'Messages', link: '#' },
+  { id: 5, label: 'Features', link: '#' },
+  { id: 6, label: 'Notification', link: '#' },
 ];
