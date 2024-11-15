16a4778733f1ee9da46c414b6d15c7455288b328
staff <masterb21052017@gmail.com>
2024-11-16 01:11:11 +0200

    keyframes postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,24 @@
 module.exports = {
-    plugins:
-      process.env.NODE_ENV === 'production'
-        ? [
-            'postcss-flexbugs-fixes',
-            [
-              'postcss-preset-env',
-              {
-                autoprefixer: {
-                  flexbox: 'no-2009',
-                },
-                stage: 3,
-                features: {
-                  'custom-properties': false,
-                },
+    plugins: process.env.NODE_ENV === 'production'
+      ? [
+          'postcss-flexbugs-fixes',
+          [
+            'postcss-preset-env',
+            {
+              autoprefixer: {
+                flexbox: 'no-2009', // для підтримки старих версій flexbox
+                grid: true, // додавання префіксів для CSS grid
               },
-            ],
-          ]
-        : [
-            // No transformations in development
+              stage: 3, // стадія підтримки CSS властивостей
+              features: {
+                'custom-properties': false, // вимикає підтримку custom properties
+                'keyframes': true, // додавання префіксів для анімацій
+              },
+            },
           ],
+        ]
+      : [
+          // No transformations in development
+        ],
   }
+  
